de9aca8f3b386d1d3eacff04065ce9eaa507e8ce
CRS <crs@CRSs-iMac-2.local>
2021-11-08 15:26:08 +0100

    initial commit

---
+++ functions/hello.js
@@ -0,0 +1,8 @@
+// domain/.netlify/functions/hello
+const items = [{ id: 1, name: "John" }];
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };
+};
